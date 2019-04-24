21e08f8989eefe8f6c243881f07d1a6246c3dd8b
akjenkins <36538399+akjenkins@users.noreply.github.com>
2019-04-23 23:13:33 -0400

    ws for 4/23, done

---
+++ build/precache-manifest.58f5f9e7a08c113e81e591162882cfc7.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "4508f477f0d259e94c0c",
+    "url": "/static/css/main.540522ad.chunk.css"
+  },
+  {
+    "revision": "4508f477f0d259e94c0c",
+    "url": "/static/js/main.ea1301a2.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "4270d9f223e41b8dbe93",
+    "url": "/static/js/2.91619cf3.chunk.js"
+  },
+  {
+    "revision": "f691ebc2100bb2c988629357f2c48d6e",
+    "url": "/index.html"
+  }
+];
